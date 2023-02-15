b4e6be218b9bf17a5bf17f153e38af26047c4271
francogallegosqu <francogallegos02@outlook.com>
2023-02-15 11:20:13 -0500

    first commit

---
+++ src/test/java/com/tech/eureka/EurekaServerVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
